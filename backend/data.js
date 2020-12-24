8465751de52ac508b609971227f6a24b5985d7f4
brian mureithi <brianmurithi65@gmail.com>
2020-12-24 12:25:53 +0300

    my first commit

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:'Grey Khaki Slim Pants',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:1300,
+            countInStock:10,
+            brand:'Lacoste',
+            rating:3.5,
+            numReviews:10,
+            desciption:'Nice high Quality pants and does not fade'
+        },
+        {
+            _id:'2',
+            name:'Adidas Checked Fit Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:1000,
+            countInStock:0,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:18,
+            desciption:'Nice high Quality Shirt'
+        },{
+        _id:'3',
+        name:'White Khaki Slim Pants',
+        category:'Pants',
+        image:'/images/p4.jpg',
+        price:1300,
+        countInStock:20,
+        brand:'Lacoste',
+        rating:4,
+        numReviews:16,
+        desciption:'Nice high Quality white khaki pants'
+    },
+    {
+        _id:'4',
+        name:' Light Blue official Shirt',
+        category:'Shirt',
+        image:'/images/p3.jpg',
+        price:1300,
+        countInStock:8,
+        brand:'Lacoste',
+        rating:5,
+        numReviews:30,
+        desciption:'Nice quality shirt'
+    },
+    {
+        _id:'5',
+        name:' Dark Grey Khaki Slim Pants',
+        category:'Pants',
+        image:'/images/p5.jpg',
+        price:'sh 1300',
+        countInStock:13,
+        brand:'Puma',
+        rating:4.5,
+        numReviews:26,
+        desciption:'Nice high Quality Pants'
+    },
+    {
+        _id:'6',
+        name:' Checked   blue sleek Shirt',
+        category:'Shirts',
+        image:'/images/p1.jpg',
+        price:1300,
+        countInStock:16,
+        brand:'Lacoste',
+        rating:3.5,
+        numReviews:23,
+        desciption:'Official Shirt'
+    },
+
+    ]
+}
+export default data; 
